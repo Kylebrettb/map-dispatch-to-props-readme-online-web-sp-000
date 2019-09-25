b5644cd359d4b91500ba54c8fb239b5f69098963
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 22:15:55 +0000

    Automatically backed up by Learn

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { addItem } from  './actions/items';
+ 
+class App extends Component {
+ 
+  handleOnClick = event => {
+    this.props.addItem()
+  }
+ 
+  render() {
+    debugger
+    return (
+      <div className="App">
+        <button onClick={this.handleOnClick}>
+          Click
+          </button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+};
+ 
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  };
+};
+ 
+export default connect(mapStateToProps, { addItem })(App);
+/* ES6 shorthand lets us pass in *one* value that will be read as the key and value */
